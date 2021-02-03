212a1848ac72cd9800783dc9ba0692c52911f1a2
Al-mamun96 <mamunkasemi5424@gmail.com>
2021-02-04 04:29:23 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,25 @@
+// undefined variable
+ 
+    // there variable no defined
+ let number;
+
+    // there function no return
+ function add (num1, num2){
+     console.log(num1+num2);
+ }
+ const result = add(12,21);
+ console.log(result);
+
+//   there pass one parameter
+function add (num1, num2){
+    console.log(num1);
+}
+const result1 = add(12);
+console.log(result1);
+
+    // object value no defined
+const person ={name:'al',phone:'01773883838'};
+console.log(person.address);
+
+const age =undefined;
+console.log(age);
