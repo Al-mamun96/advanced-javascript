0904cb1dc113c84885b34d4465e34c3674636050
Al-mamun96 <mamunkasemi5424@gmail.com>
2021-02-04 04:15:38 +0600

    true-false-conditions-check

---
+++ true-false.js
@@ -0,0 +1,19 @@
+// falsy: condition
+// 01 false
+// 02 undefined
+// 03 null
+// 04 NaN
+// 05 0
+// 06 ""
+
+ // truthy condition
+// 01 except 0
+// 02 " "
+
+const name = "mamun";
+if (name.length>0) {
+    console.log('this is true');
+} else {
+    console.log('this is false');
+    
+}
